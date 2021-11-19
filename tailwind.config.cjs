2fd237be1e5c2cabb3ebb900efe39963c2609f3c
Meowmori Aoi <meowmoriaoi@gmail.com>
2021-11-19 15:29:03 +0700

    Change config.js to config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  mode:'jit',
+  purge: [
+    './src/**/*.html',
+    './src/routes/**/*.svelte'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
